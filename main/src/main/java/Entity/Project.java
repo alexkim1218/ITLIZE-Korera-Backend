89ef767700d04957b0c3ab420fd536ff1b85f88f
christhoperbernard <chbernar@ucsd.edu>
2019-10-11 17:03:57 -0400

    added entity (duo chris)

--- main/src/main/java/Entity/Project.java
+++ main/src/main/java/Entity/Project.java
@@ -1,12 +1,42 @@
 package Entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
 import org.springframework.data.annotation.Id;
 
+@Entity
+@Table(name = "Project")
 public class Project {
 	
 	@Id
-	private int Pid;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int projectID;
 	
-	private String name;
+	@Column
+	private String projectName;
+	
+	public int getProjectID() {
+		return projectID;
+	}
 
+	public void setProjectID(int projectID) {
+		this.projectID = projectID;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Project Name: " + projectName;
+	}
 }
--- main/src/main/java/Entity/Resource.java
+++ main/src/main/java/Entity/Resource.java
@@ -1,9 +1,52 @@
 package Entity;
 
-public class Resource {
-	@org.springframework.data.annotation.Id
-	private int Id;
-	private String resourceName;
-	private  int resourceCode;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "Resource")
+public class Resource {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int resourceID;
+	
+	@Column
+	private String resourceName;
+	
+	@Column
+	private int resourceCode;
+
+	public int getResourceID() {
+		return resourceID;
+	}
+
+	public void setResourceID(int resourceID) {
+		this.resourceID = resourceID;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
+	public int getResourceCode() {
+		return resourceCode;
+	}
+
+	public void setResourceCode(int resourceCode) {
+		this.resourceCode = resourceCode;
+	}
+	
+	@Override
+	public String toString() {
+		return  "Resource Code: " + resourceCode + " | Name: " + resourceName;
+	}
 }
