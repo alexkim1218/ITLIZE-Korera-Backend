37414bf22d77c8eaa87519dbb3c9841d0795df40
christhoperbernard <chbernar@ucsd.edu>
2019-10-11 11:35:22 -0400

    test slack notification 2

--- main/src/main/java/com/korera/main/MainApplication.java
+++ main/src/main/java/com/korera/main/MainApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
 	public static void main(String[] args) {
-//		test slack
+//		test slack 2
 		SpringApplication.run(MainApplication.class, args);
 	}
 
