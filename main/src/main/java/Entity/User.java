bb0b5c387a13e13ea60f667cfd9c21b5849c9668
Alex Kim <alexkim1218@yahoo.com>
2019-10-15 19:26:17 -0400

    Overall Updates
    - Removed unnecessary methods
    - Renamed some methods and variables to match the UML diagram

--- main/src/main/java/Entity/User.java
+++ main/src/main/java/Entity/User.java
@@ -11,80 +11,47 @@ import org.springframework.context.support.BeanDefinitionDsl.Role;
 public class User {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long user_id;
+	private Long userID;
 
 	@NaturalId
 	@NotBlank
 	private String username;
 
 	@NotBlank
-	private String first_name;
-
-	@NotBlank
-	private String last_name;
-
-	@NotBlank
 	private String password;
+
+	@NotBlank
+	private String name;
 
 	// TODO: CREATE MANY-TO-MANY TABLE
 
 	public User() {}
 
-	public User (String username, String first_name, String last_name, String password) {
+	public User (String username, String password, String name) {
 		this.username = username;
-		this.first_name = first_name;
-		this.last_name = last_name;
 		this.password = password;
+		this.name = name;
 	}
 
-	public Long getId () {
-		return user_id;
-	}
-
-	public void setId (Long ser_id) {
-		this.user_id = ser_id;
+	public Long getID () {
+		return userID;
 	}
 
 	public String getUsername () {
 		return username;
 	}
 
-	public void setUsername (String username) {
-		this.username = username;
-	}
-
-	public String getFirst_name () {
-		return first_name;
-	}
-
-	public void setFirst_name (String first_name) {
-		this.first_name = first_name;
-	}
-
-	public String getLast_name () {
-		return last_name;
-	}
-
-	public void setLast_name (String last_name) {
-		this.last_name = last_name;
-	}
-
 	public String getPassword () {
 		return password;
 	}
 
-	public void setPassword (String password) {
-		this.password = password;
+	public String getName () {
+		return name;
 	}
 
-	public Set<Role> getRoles() {
+	public Set<Role> getTitle() {
 		// TODO: CHANGE RETURN VALUE AFTER CREATING TABLE
 		return null;
 		// return roles;
-	}
-
-	public void setRoles(Set<Role> roles) {
-		// TODO: ADD RETURN VALUE AFTER CREATING TABLE
-		// this.roles = roles;
 	}
 }
