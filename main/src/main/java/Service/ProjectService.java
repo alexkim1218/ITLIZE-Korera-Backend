a1a41a37ef5e91068f813ac03b9efa7dea24d0c8
yangtree21st <yangtree21st@gmail.com>
2019-10-16 14:47:50 -0400

    finished Projectservice Interface

--- main/src/main/java/Service/ProjectService.java
+++ main/src/main/java/Service/ProjectService.java
@@ -1,5 +1,22 @@
 package Service;
 
+import DTO.ProjectDTO;
+import Entity.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface ProjectService {
+      ProjectDTO getDefaultProjectByUserID(Integer userId);
+
+      Page<Resource> getResourcesbyPid(Integer pId, Pageable pageable);
+      List<Resource>  setResouresbyPid(Integer pId);
+
+      List<ProjectDTO> getProjectsByUid(Integer uId);
+
+      void addColumn (Integer pId, String colName, String colType);
+
+
 
 }
