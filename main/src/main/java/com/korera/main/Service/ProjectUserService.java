7120f265a70bb5a6ca33f0845bc415d258369dd4
christhoperbernard <chbernar@ucsd.edu>
2019-10-22 15:45:50 -0400

    fix some configuration bugs

--- main/src/main/java/com/korera/main/Service/ProjectUserService.java
+++ main/src/main/java/com/korera/main/Service/ProjectUserService.java
@@ -3,6 +3,9 @@ package com.korera.main.Service;
 import com.korera.main.Entity.ProjectUser;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface ProjectUserService {
 	List<ProjectUser> getAllProjectUser(Integer uid);
 }
